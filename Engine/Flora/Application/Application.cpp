8c1bdf85fb6a643a3047b9b329acf62c235f4e20
chgalante <34777014+chgalante@users.noreply.github.com>
2021-12-30 14:25:20 -0500

    Check that required GLFW extensions are supported by the vulkan instance

--- Engine/Flora/Application/Application.cpp
+++ Engine/Flora/Application/Application.cpp
@@ -13,15 +13,13 @@ Application::~Application() {
   vkDestroyInstance(mVkInstance, nullptr);
 }
 
-void Application::Run() {
+static VkInstance InitVulkanInstance() {
+  VkInstance   vkInstance;
   uint32_t     glfwExtensionCount = 0;
   const char **glfwExtensions =
       glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
   uint32_t                           extensionCount = 0;
   std::vector<VkExtensionProperties> extensions;
-
-  /* Init Application Window */
-  mWindow = CreateScope<Window>();
 
   /* Init Vulkan */
   FE_CORE_TRACE("Initializing Vulkan Instance...");
@@ -40,7 +38,7 @@ void Application::Run() {
   createInfo.ppEnabledExtensionNames = glfwExtensions;
   createInfo.enabledLayerCount       = 0;
 
-  if (vkCreateInstance(&createInfo, nullptr, &mVkInstance) != VK_SUCCESS) {
+  if (vkCreateInstance(&createInfo, nullptr, &vkInstance) != VK_SUCCESS) {
     throw std::runtime_error("failed to create vulkan instance!");
   }
 
@@ -50,10 +48,39 @@ void Application::Run() {
   vkEnumerateInstanceExtensionProperties(nullptr,
                                          &extensionCount,
                                          extensions.data());
-  FE_CORE_TRACE("Available extensions: ");
-  for (const auto &extension : extensions) {
-    FE_CORE_TRACE("\t{0}", extension.extensionName);
+  FE_CORE_TRACE("Validating required extensions... ");
+
+  /* Check that required GLFW extensions are supported */
+  bool bExtensionsAreSupported = true;
+  for (auto i = 0; i < glfwExtensionCount; ++i) {
+    bool bExtensionSupportFound = false;
+    for (const auto &supportedExtension : extensions) {
+      if (strcmp(glfwExtensions[i], supportedExtension.extensionName)) {
+        bExtensionSupportFound = true;
+        break;
+      }
+    }
+    if (bExtensionSupportFound) {
+      FE_CORE_INFO("\t [*] {0}", glfwExtensions[i]);
+    } else {
+      FE_CORE_ERROR("\t [!] {0}", glfwExtensions[i]);
+      bExtensionsAreSupported = false;
+    }
   }
+  if (!bExtensionsAreSupported) {
+    throw std::runtime_error("One or more required extensions are not "
+                             "supported by the current vulkan instance!");
+  }
+  return vkInstance;
+}
+
+void Application::Run() {
+
+  /* Init Application Window */
+  mWindow = CreateScope<Window>();
+
+  /* Init Vulkan Instance */
+  mVkInstance = InitVulkanInstance();
 
   /* Attach each of the application layers */
   for (int32_t idx = mLayers->size() - 1; idx >= 0; idx--) {
